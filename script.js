35959860c7d61fc7473447951838b2103fa00ced
FrankmDev <frankmmarindev@gmail.com>
2023-09-26 10:11:01 +0200

    More Features

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const inputElement = document.querySelectorAll(".input");
+const inputLabel = document.querySelectorAll(".input-label");
+
+inputElement.forEach((input, index) => {
+  input.addEventListener("input", function () {
+    if (inputElement.textContent !== "") {
+      inputLabel[index].classList.add("has-content");
+      inputElement[index].style.outline = "solid 1px #bc7af9";
+    } else {
+      inputLabel[index].classList.remove("has-content");
+    }
+  });
+});
+
+if (input.value !== "") {
+  inputLabel[index].classList.add("has-content");
+}
